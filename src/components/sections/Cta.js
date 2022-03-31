41d4e6199b258f318c0bad1d0f48faf9b2b2f457
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2022-03-30 20:55:29 -0500

    success toast

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -1,116 +1,20 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import classNames from 'classnames';
-// import { SectionProps } from '../../utils/SectionProps';
-// import Input from '../elements/Input';
-// import Button from '../elements/Button';
-// import SectionHeader from './partials/SectionHeader';
-
-// const propTypes = {
-//   ...SectionProps.types,
-//   split: PropTypes.bool
-// }
-
-// const defaultProps = {
-//   ...SectionProps.defaults,
-//   split: false
-// }
-
-// const Cta = ({
-//   className,
-//   topOuterDivider,
-//   bottomOuterDivider,
-//   topDivider,
-//   bottomDivider,
-//   hasBgColor,
-//   invertColor,
-//   split,
-//   ...props
-// }) => {
-
-// const outerClasses = classNames(
-//   'cta section center-content-mobile reveal-from-bottom',
-//   topOuterDivider && 'has-top-divider',
-//   bottomOuterDivider && 'has-bottom-divider',
-//   hasBgColor && 'has-bg-color',
-//   invertColor && 'invert-color',
-//   className
-// );
-
-// const innerClasses = classNames(
-//   'testimonial-inner section-inner',
-//   topDivider && 'has-top-divider',
-// );  
-
-// const sectionHeader = {
-//   title: 'Contact Us',
-//   paragraph: 'We\'d love to hear about your project.'
-// };
-
-// const encode = (data) => {
-//   return Object.keys(data)
-//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//       .join("&");
-// }
-
-// handleSubmit = e => {
-//   fetch("/", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: encode({ "form-name": "contact", ...this.state })
-//   })
-//     .then(() => alert("Success!"))
-//     .catch(error => alert(error));
-
-//   e.preventDefault();
-// };
-
-// handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-
-//   return (
-//     <section
-//       {...props}
-//       className={outerClasses}
-//     >
-//       <div className="container">
-//         <div className={innerClasses}>
-//           <SectionHeader data={sectionHeader} className="center-content" />
-//           <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" >
-//             <Input>
-//             {/* <p class="hidden">
-//               <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
-//             </p> */}
-//             <Input type="text" placeholder="Full name*" name="name" />   
-//             </Input>
-//             <Input>
-//               <Input type="email" placeholder="Email*" name="email" />
-//             </Input>
-//             <Input>
-//               <textarea name="message" placeholder="Message*" ></textarea>
-//             </Input>
-//             <Button type="submit">Submit</Button>
-//           </form>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// Cta.propTypes = propTypes;
-// Cta.defaultProps = defaultProps;
-
-// export default Cta;
-
 import React from 'react';
 import Input from '../elements/Input';
 import Button from '../elements/Button';
+import SectionHeader from './partials/SectionHeader';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const encode = (data) => {
   return Object.keys(data)
       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
 }
+
+const sectionHeader = {
+  title: 'Tell Us About Your Project',
+  paragraph: ''
+};
 
 class Cta extends React.Component {
   constructor(props) {
@@ -124,7 +28,16 @@ class Cta extends React.Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => alert("Success!"))
+      .then(() => toast.success("Thank you! We'll get back to you shortly.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        }))
+      // .then(() => alert('Success'))
       .catch(error => alert(error));
 
     e.preventDefault();
@@ -135,23 +48,34 @@ class Cta extends React.Component {
   render() {
     const { name, email, message } = this.state;
     return (
-      <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={this.handleSubmit} >
-        <div>
-        <p class="hidden">
-          <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
-        </p>
-        <Input type="text" placeholder="Full name*" name="name" value={name} onChange={this.handleChange} />   
-        </div>
-        <div>
-          <Input type="email" placeholder="Email*" name="email" value={email} onChange={this.handleChange} />
-        </div>
-        <div>
-          <textarea name="message" placeholder="Message*" value={message} onChange={this.handleChange} ></textarea>
-        </div>
-        <div>
-          <Button type="submit">Submit</Button>
-        </div>
-      </form>
+      <div className="container" style={{marginTop: '100px', width: '60%'}}>
+        <SectionHeader data={sectionHeader} className="center-content" />
+        <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={this.handleSubmit} >
+          <div>
+          <p className="hidden">
+            <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+          </p>
+          <Input type="text" placeholder="Full name*" name="name" value={name} onChange={this.handleChange} />   
+          </div>
+          <div>
+            <Input type="email" placeholder="Email*" name="email" value={email} onChange={this.handleChange} />
+          </div>
+          <div>
+            <select style={{width: '100%', height: '50px'}} value={this.state.value} onChange={this.handleChange}>
+              <option disabled selected>I'm interested in...</option>
+              <option value="thirty">30-minute consulting session</option>
+              <option value="discovery">Discovery call to learn more about long term consulting partnerships</option>
+            </select>
+          </div>
+          <div>
+            <Input type="textarea" name="message" placeholder="Message*" value={message} onChange={this.handleChange} />
+          </div>
+          <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
+            <Button color="primary" type="submit">Submit</Button>
+            <ToastContainer />
+          </div>
+        </form>
+      </div>
     );
   }
 }
