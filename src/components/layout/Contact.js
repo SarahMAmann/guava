da0ee6fa5e6525109b660162ef93f9fb8c9f4da8
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2022-04-03 23:06:44 -0500

    progress

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -68,68 +68,65 @@ class Contact extends React.Component {
 
   render() {
     // const { name, message } = this.state;
-    // const options = [
-    //     {
-    //       name: '30-minute consulting session',
-    //       value: 'thirty'
-    //     },
-    //     {
-    //       name: 'Discovery call to learn more about long term consulting partnerships',
-    //       value: 'discovery'
-    //     }
-    //   ];
+    const options = [
+        {
+          name: '30-minute consulting session',
+          value: 'thirty'
+        },
+        {
+          name: 'Discovery call to learn more about long term consulting partnerships',
+          value: 'discovery'
+        }
+      ];
     return (
-        // <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" onSubmit={this.handleSubmit} >
-        //     <input type="hidden" name="form-name" value="contact" />
-        //   <div>
-        //     <p className="hidden">
-        //         <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
-        //     </p>
-        //     <Input style={{borderRadius: '4px'}} type="text" placeholder="Name" name="name" id="name" required value={name} onChange={this.handleChange} />   
-        //   </div>
-        //   <div style={{marginTop: '20px'}}>
-        //     <Input style={{borderRadius: '4px'}} type="text" placeholder="Message" name="message" id="message" required value={message} onChange={this.handleChange} />
-        //   </div>
-        //   {/* <div style={{marginTop: '20px'}}>
-        //     <Input style={{borderRadius: '4px'}} type="text" placeholder="Location/Timezone" name="timezone" required value={timezone} onChange={this.handleChange} />
-        //   </div>
-        //   <div style={{marginTop: '20px'}}>
-        //     <select name="callType" required onChange={this.handleChange} value={callType} style={{width: '100%', height: '50px', borderRadius: '4px'}}>
-        //       <option value="" disabled>I'm interested in...</option>
-        //         {options.map(item => (
-        //         <option key={item.value} value={item.value}>
-        //           {item.name}
-        //         </option>
-        //       ))}
-        //     </select>
-        //   </div> */}
-        //   {/* <div style={{marginTop: '20px'}}>
-        //     <Input style={{borderRadius: '4px'}} type="textarea" name="message" placeholder="Message" required value={message} onChange={this.handleChange} />
-        //   </div> */}
-        //   <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
-        //     <Button color="primary" type="submit">Submit</Button>
-        //     <ToastContainer />
-        //   </div>
-        // </form>
+        <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" >
+            <input type="hidden" name="form-name" value="contact" />
+          <div>
+            <p className="hidden">
+                <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+            </p>
+            <Input style={{borderRadius: '4px'}} type="text" placeholder="Name" name="name" id="name" required />   
+          </div>
+          <div style={{marginTop: '20px'}}>
+            <Input style={{borderRadius: '4px'}} type="text" placeholder="Location/Timezone" name="timezone" id="timezone" required />
+          </div>
+          <div style={{marginTop: '20px'}}>
+            <select name="callType" id="callType" required style={{width: '100%', height: '50px', borderRadius: '4px'}}>
+              <option value="" disabled>I'm interested in...</option>
+                {options.map(item => (
+                <option key={item.value} value={item.value}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div style={{marginTop: '20px'}}>
+            <Input style={{borderRadius: '4px'}} type="textarea" name="message" id="message" placeholder="Message" required />
+          </div>
+          <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
+            <Button color="primary" type="submit" value="Submit message">Submit</Button>
+            <ToastContainer />
+          </div>
+        </form>
 
-        <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label htmlFor="name">Name</label> <br />
-          <input type="text" id="name" name="name" required />
-        </p>
-        <p>
-          <label htmlFor="email">Email</label> <br />
-          <input type="email" id="email" name="email" required />
-        </p>
-        <p>
-          <label htmlFor="message">Message</label> <br />
-          <textarea id="message" name="message" required></textarea>
-        </p>
-        <p>
-          <input type="submit" value="Submit message" />
-        </p>
-      </form>
+    //     <form name="contact" method="post">
+    //     <input type="hidden" name="form-name" value="contact" />
+    //     <p>
+    //       <label htmlFor="name">Name</label> <br />
+    //       <input type="text" id="name" name="name" required />
+    //     </p>
+    //     <p>
+    //       <label htmlFor="email">Email</label> <br />
+    //       <input type="email" id="email" name="email" required />
+    //     </p>
+    //     <p>
+    //       <label htmlFor="message">Message</label> <br />
+    //       <textarea id="message" name="message" required></textarea>
+    //     </p>
+    //     <p>
+    //       <input type="submit" value="Submit message" />
+    //     </p>
+    //   </form>
     );
   }
 }
