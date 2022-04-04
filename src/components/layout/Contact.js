c00cbd8717663b424d5133699747da3b8f93c1b0
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2022-04-03 23:30:49 -0500

    making select better

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -1,80 +1,18 @@
 import React from 'react';
 import Input from '../elements/Input';
 import Button from '../elements/Button';
-// import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-// const encode = (data) => {
-//     return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// }
-
-// const createFormDataObj = data => {
-//     const formData = new FormData();
-//     Object.keys(data).forEach(k => {
-//       formData.append(k, data[k]);
-//     });
-//     return formData;
-//   };
+import Select from 'react-select';
 
 
 class Contact extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     // this.state = { name: "", message: "" };
-//   }
-
-//   handleSubmit = e => {
-//       console.log(this.state);
-//       const data = {
-//         'form-name': 'contact-us',
-//         name: this.state.name,
-//         message: this.state.message,
-//       };
-//     fetch("/", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//       body: new URLSearchParams(createFormDataObj(data)).toString(),
-//     //   body: encode({ "form-name": "contact", ...this.state })
-//     })
-//       .then(() => toast.success("Thank you! We'll get back to you shortly.", {
-//         position: "top-center",
-//         autoClose: 5000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//         }))
-//       .catch(error => toast.error("Sorry, we're having trouble processing your request.", {
-//         position: "top-center",
-//         autoClose: 5000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//         }));
-
-//     e.preventDefault();
-//     this.setState({
-//       name: '',
-//       message: ''
-//     });
-//   };
-
-//   handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
   render() {
-    // const { name, message } = this.state;
     const options = [
         {
-          name: '30-minute consulting session',
+          label: '30-minute consulting session',
           value: 'thirty'
         },
         {
-          name: 'Discovery call to learn more about long term consulting partnerships',
+          label: 'Discovery call to learn more about long term consulting partnerships',
           value: 'discovery'
         }
       ];
@@ -88,45 +26,21 @@ class Contact extends React.Component {
             <Input style={{borderRadius: '4px'}} type="text" placeholder="Name" name="name" id="name" required />   
           </div>
           <div style={{marginTop: '20px'}}>
+            <Input style={{borderRadius: '4px'}} type="email" placeholder="Email" name="email" id="timezone" required />
+          </div>
+          <div style={{marginTop: '20px'}}>
             <Input style={{borderRadius: '4px'}} type="text" placeholder="Location/Timezone" name="timezone" id="timezone" required />
           </div>
           <div style={{marginTop: '20px'}}>
-            <select name="callType" id="callType" required style={{width: '100%', height: '50px', borderRadius: '4px'}}>
-              <option value="" disabled>I'm interested in...</option>
-                {options.map(item => (
-                <option key={item.value} value={item.value}>
-                  {item.name}
-                </option>
-              ))}
-            </select>
+            <Select name="callType" id="callType" required options={options} placeholder={'I\'m interested in...'} />
           </div>
           <div style={{marginTop: '20px'}}>
             <Input style={{borderRadius: '4px'}} type="textarea" name="message" id="message" placeholder="Message" required />
           </div>
           <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '10px'}}>
             <Button color="primary" type="submit" value="Submit message">Submit</Button>
-            {/* <ToastContainer /> */}
           </div>
         </form>
-
-    //     <form name="contact" method="post">
-    //     <input type="hidden" name="form-name" value="contact" />
-    //     <p>
-    //       <label htmlFor="name">Name</label> <br />
-    //       <input type="text" id="name" name="name" required />
-    //     </p>
-    //     <p>
-    //       <label htmlFor="email">Email</label> <br />
-    //       <input type="email" id="email" name="email" required />
-    //     </p>
-    //     <p>
-    //       <label htmlFor="message">Message</label> <br />
-    //       <textarea id="message" name="message" required></textarea>
-    //     </p>
-    //     <p>
-    //       <input type="submit" value="Submit message" />
-    //     </p>
-    //   </form>
     );
   }
 }
