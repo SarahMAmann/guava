61dbd6d74e29d3708fe15df4de90bef8bf51e48a
Sarah Amann <sarah@Sarahs-MacBook-Pro.local>
2022-04-03 19:30:33 -0500

    testing

--- src/views/Home.js
+++ src/views/Home.js
@@ -15,21 +15,12 @@ const Home = () => {
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       <Testimonial topDivider />
       {/* <Cta /> */}
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" netlify>
         <p>
-          <label>Your Name: <input type="text" name="name" /></label>
+          <label>Name <input type="text" name="name" /></label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>Email <input type="email" name="email" /></label>
         </p>
         <p>
           <button type="submit">Send</button>
